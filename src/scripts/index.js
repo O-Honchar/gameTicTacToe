baff05e930823ecff883f6b6ba4c83f4ac9b5e09
Oleh Honchar <O.O.Honchar@gmail.com>
2021-05-25 19:19:26 +0300

    feat: add event listener for start button and call checkFirstTurn function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,13 +2,16 @@
 
 import './../styles/main.scss';
 import Player from './classPlayer';
-// import checkFirstTurn from './randomizer.js'
+import checkFirstTurn from './randomizer.js'
 // import random from './randomize';
 
 const player1Input = document.querySelector('#player1');
 const player2Input = document.querySelector('#player2');
 const player1 = new Player(player1Input.placeholder.trim());
 const player2 = new Player(player2Input.placeholder.trim());
+const start = document.querySelector('#start');
+const min = 1;
+const max = 2;
 
 function player1InputHandler() {
     player1.name = player1Input.value.trim();
@@ -20,5 +23,10 @@ function player2InputHandler() {
     console.log('player2: ', player2);
 }
 
+function startHandler() {
+    checkFirstTurn(min, max);    
+}
+
 player1Input.addEventListener('change', player1InputHandler);
 player2Input.addEventListener('change', player2InputHandler);
+start.addEventListener('click', startHandler);
