d51e678d1569a18538e73a54d43ea9e28cb01427
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-27 20:25:59 +0300

    fix: delete experimentsl code? that I forget to delete

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,17 +6,13 @@ import { gameFieldHandler } from './gameField';
 import { Player, player1, player2 } from './classPlayer';
 import checkFirstTurn from './randomizer.js';
 import addPlayerSign from './addPlayerSign.js';
-import highlightPlayer from './highlightPlayer.js'
+import highlightPlayer from './highlightPlayer.js';
 // import random from './randomize';
 
 export const gameFieldDiv = document.querySelector('#game-field');
 export const player1Input = document.querySelector('#player1');
 export const player2Input = document.querySelector('#player2');
 const start = document.querySelector('#start');
-
-export default function returnGameFieldDiv() {
-  return gameFieldDiv;
-}
 
 function player1InputHandler() {
   player1.name = player1Input.value.trim();
