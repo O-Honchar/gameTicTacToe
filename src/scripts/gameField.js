84818492bdc614f8397aed9f5785919ba19ec2ba
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-27 15:56:04 +0300

    feat: add array with checked cells by user, add reseter fo this array, add check if cell was checked before

--- src/scripts/gameField.js
+++ src/scripts/gameField.js
@@ -1,10 +1,30 @@
 import { Player, player1, player2 } from './classPlayer';
 
+const cellArray = [];
+
+const addSignToArr = (cellNum, sign) => {
+  if (!cellArray[cellNum - 1]) {
+    cellArray[cellNum - 1] = sign;
+  }
+};
+
+const addSignToCell = (e, cellNum, sign) => {
+  if (!cellArray[cellNum - 1]) {
+    e.target.textContent = `${sign}`;
+  }
+};
+
+// const resetCellArray = () => {
+//   cellArray.length = 0;
+// };
+
 export default (e) => {
-  // const cellNumber = e.target.dataset.cell;
-  console.log(Player.active);
-  e.target.textContent = `${Player.active.sign}`;
-  // console.log(cellNumber);
+  const sign = Player.active.sign;
+  const cellNumber = e.target.dataset.cell;
+
+  addSignToCell(e, cellNumber, sign);
+  addSignToArr(cellNumber, sign);
+  console.log(cellArray);
 
   Player.toggleActive(player1, player2);
 };
