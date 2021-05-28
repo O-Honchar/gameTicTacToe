6f5f126cc79c7e8cfd2b7b7e29ac7cf7031a0ec5
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-28 12:52:04 +0300

    feat: add function to check victory in rows and columns

--- src/scripts/gameField.js
+++ src/scripts/gameField.js
@@ -1,8 +1,10 @@
 import { Player, player1, player2 } from './classPlayer';
 import { gameFieldDiv } from './index';
-// const gameFieldDiv = document.querySelector('#game-field');
 
 const cellArray = [];
+const cellArrayLength = 9,
+  gameFieldSideLength = 3;
+cellArray.length = cellArrayLength;
 
 const addSignToArr = (cellNum, sign) => {
   if (!cellArray[cellNum - 1]) {
@@ -18,6 +20,9 @@ const addSignToCell = (e, cellNum, sign) => {
 
 export const resetCellArray = () => {
   cellArray.length = 0;
+  for (let i = 0; i < cellArrayLength; i++) {
+    cellArray.push(i);
+  }
 };
 
 export const resetField = () => {
@@ -27,12 +32,51 @@ export const resetField = () => {
   });
 };
 
+const checkResult = () => {
+  // ================== horizontally
+  for (let i = 0; i < cellArrayLength; i = i + gameFieldSideLength) {
+    if (
+      cellArray[i] === cellArray[i + 1] &&
+      cellArray[i + 1] === cellArray[i + 2] &&
+      cellArray[i] === cellArray[i + 2] &&
+      cellArray[i]
+    ) {
+      console.log(`we find victory on row # ${i / gameFieldSideLength}`);
+      console.log(
+        `Player ${Player.active.name} win! His sign is ${Player.active.sign}`
+      );
+      break;
+    }
+  }
+
+  // ================== vertically
+  console.log('========================');
+  for (let i = 0; i < gameFieldSideLength; i++) {
+    if (
+      cellArray[i] === cellArray[i + 3] &&
+      cellArray[i + 3] === cellArray[i + 6] &&
+      cellArray[i] === cellArray[i + 6] &&
+      cellArray[i]
+    ) {
+      console.log(`we find victory on col # ${i}`);
+      console.log(
+        `Player ${Player.active.name} win! His sign is ${Player.active.sign}`
+      );
+      break;
+    }
+  }
+
+  // ================== diagonally
+
+  console.log(cellArray);
+};
+
 export const gameFieldHandler = (e) => {
   const sign = Player.active.sign;
   const cellNumber = e.target.dataset.cell;
 
   addSignToCell(e, cellNumber, sign);
   addSignToArr(cellNumber, sign);
-
+  checkResult();
   Player.toggleActive(player1, player2);
 };
